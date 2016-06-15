fd03b0cb1da618383a26a54e24454f125ee4ae2f
Christian Bodart <chris@bodar.com>
2016-06-15 15:31:18 +0100

    basic tests defined (and working)

--- tests/spec/micronic.spec.js
+++ tests/spec/micronic.spec.js
@@ -4,12 +4,12 @@ describe("micronic", function() {
       classNames = ["none","nano","micro","mini","norm","high","ultra"],
       classWidths = {
         none: [0,0],
-        nano: [0,96],
+        nano: [1,96],
         micro: [97,288],
         mini: [289,768],
         norm: [769,1200],
         high: [1201,1600],
-        ultra: [1601]
+        ultra: [1601,10000]
       };
   function setDimensions(width,height) {
     if (typeof width != "undefined") container.style.width = width;
@@ -18,22 +18,47 @@ describe("micronic", function() {
 
   beforeEach(function() {
     loadFixtures('init.html')
-    container = document.querySelector("#container")
-    target=document.querySelector("#container [micronic]")
+    
+    target=document.querySelector("#container [micronic]");
+    container = target.offsetParent
+    micronic.init()
 
   });
-  //it("should set default classnames appropriately", function () {
     classNames.forEach(function (className) {
-         it("should set "+className+" as a classname when the container is "+classWidths[className][1]+"px wide", function () {
-          setDimensions(classWidths[className]+"px");
-          expect(container.classList.contains(className)).toBe(true);
-
-
-         
+         it("should set "+className+" as a classname when the container is "+classWidths[className][1]+"px wide", function (done) {
+          setDimensions(classWidths[className][0]+"px");
+          setTimeout(function () {
+            expect(target.className).toBe(className);
+            setTimeout(done,100)
+          },50)
       })
-
-  
   });
+    it("should not fire on a non-micronic element", function (done) {
+      var div = document.createElement("div")
+      div.className = "other"
+      document.body.appendChild(div)
+      setTimeout(function () {
+        expect(div.className).toBe("other");
+        setTimeout(done,100)
+      },50)
+    });
+    it("should be able to add inline class definitions", function (done) {
+      var custom = document.querySelector("#custom");
+      setTimeout(function () {
+        //console.log(custom.className)
+      expect(custom.classList.contains("custom")).toBe(true)
+      expect(custom.classList.contains("nano")).toBe(true)
+              setTimeout(done,100)
+            },50)
+    });
+    it("should be able to disable default additions", function (done) {
+      var custom = document.querySelector("#customDefault");
+      setTimeout(function () {
+      expect(custom.classList.contains("nano")).toBe(false)
+              setTimeout(done,100)
+            },50)
+
+    })
   
  
 
--- tests/src/micronic.js
+++ tests/src/micronic.js
@@ -1,8 +1,61 @@
 this.micronic = {
+	document: this.document,
+	prefixes: ["webkit", "moz", "MS", "o", ""],
+	classNames:["none","nano","micro","mini","norm","high","ultra"],
+	classSizes: {
+        none: [0,0],
+        nano: [1,96],
+        micro: [97,288],
+        mini: [289,768],
+        norm: [769,1200],
+        high: [1201,1600],
+        ultra: [1601,10000]
+    },
+    addClass: function (name,dimensions) {
+    	this.classNames.push(name);
+    	this.classSizes[name] = dimensions;
+    },
 	init: function () {
-		this.attachEvents()
-		this.injectStyleSheet();
+		this.attachEvents();
+	},
+	attachEvents: function () {
+		for (var p = 0, prefixes = this.prefixes, type = "AnimationStart"; p < prefixes.length; p++) {
+			if (!prefixes[p]) type = type.toLowerCase();
+			window.addEventListener(prefixes[p]+type, this, true);
+		}
+	},
+	handleEvent: function (event) {
+		var target = event.target;
+		if (target.hasAttribute("micronic")) {
+			var container = target.offsetParent;
+			var width = container.getBoundingClientRect().width,
+				classList = target.classList
+			var customDefs = target.getAttribute("micronic")
+			if (customDefs) {
+				var customDefinitions = Function("return {"+customDefs+"}")();
+				var disableDefault = customDefinitions.default === false;
+				delete customDefinitions.default;
+				Object.keys(customDefinitions).forEach(this.determineClassBySize.bind(this,target,classList,width,customDefinitions))
+
+			}
+			console.log(disableDefault)
+			if (!disableDefault) {
+			this.classNames.forEach(this.determineClassBySize.bind(this,target,classList,width,this.classSizes))
+	
+			}
+			
+		}
+	},
+	determineClassBySize: function (target,classList,width,classSizes,className) {
+		var dimensions = classSizes[className];
+		if (width>=dimensions[0] && width <= dimensions[1]) {
+			classList.add(className)
+			console.log(className,dimensions,width)
+		} else {
+			classList.remove(className)
+		}
 	}
+	
 
 
 }
