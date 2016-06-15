91d3c6ee8225d17fd839050be644a32511095071
Christian Bodart <chris@bodar.com>
2016-06-15 23:05:18 +0100

    refactor and renaming

--- tests/spec/micronic.spec.js
+++ tests/spec/micronic.spec.js
@@ -1,19 +1,27 @@
 describe("micronic", function() {
   var container,
       target,
-      classNames = ["none","nano","micro","mini","norm","high","ultra"],
+      classNames = ["zero-width","xx-small-width","x-small-width","small-width","medium-width","large-width","x-large-width","xx-large-width"],
       classWidths = {
-        none: [0,0],
-        nano: [1,96],
-        micro: [97,288],
-        mini: [289,768],
-        norm: [769,1200],
-        high: [1201,1600],
-        ultra: [1601,10000]
-      };
+        "zero-width": [0,0],
+        "xx-small-width": [1,96],
+        "x-small-width": [97,288],
+        "small-width": [289,768],
+        "medium-width": [769,1200],
+        "large-width": [1201,1600],
+        "x-large-width": [1601,2560],
+        "xx-large-width": [2561,10000]
+    };
   function setDimensions(width,height) {
     if (typeof width != "undefined") container.style.width = width;
     if (typeof height != "undefined") container.style.height = height;
+  }
+
+  function mockResize(width,height) {
+    document.body.style.width = width? width +"px" : "";
+    document.body.style.height = height? height +"px" : "";
+    var resizeEvent = new Event("resize")
+    window.dispatchEvent(resizeEvent)
   }
 
   beforeEach(function() {
@@ -24,6 +32,9 @@ describe("micronic", function() {
     micronic.init()
 
   });
+  afterEach(function () {
+    setDimensions("")
+  })
     classNames.forEach(function (className) {
          it("should set "+className+" as a classname when the container is "+classWidths[className][1]+"px wide", function (done) {
           setDimensions(classWidths[className][0]+"px");
@@ -47,17 +58,33 @@ describe("micronic", function() {
       setTimeout(function () {
         //console.log(custom.className)
       expect(custom.classList.contains("custom")).toBe(true)
-      expect(custom.classList.contains("nano")).toBe(true)
+      expect(custom.classList.contains("xx-small-width")).toBe(true)
               setTimeout(done,100)
             },50)
     });
     it("should be able to disable default additions", function (done) {
       var custom = document.querySelector("#customDefault");
       setTimeout(function () {
-      expect(custom.classList.contains("nano")).toBe(false)
+      expect(custom.classList.contains("xx-small-width")).toBe(false)
               setTimeout(done,100)
             },50)
 
+    });
+    it("should be able to update classes when the window is resized", function (done) {
+      var scaling = document.querySelector("#scaling");
+      mockResize(400,400)
+      setTimeout(function () {
+        expect(scaling.className).toBe("small-width");
+        mockResize("1000","1000")
+        setTimeout(function () {
+          expect(scaling.className).toBe("medium-width");
+          done()
+        },50)
+        
+      },50)
+    });
+    it("should be able to update classes if reflow detection is enabled", function () {
+      
     })
   
  
--- tests/src/micronic.js
+++ tests/src/micronic.js
@@ -1,15 +1,16 @@
 this.micronic = {
 	document: this.document,
 	prefixes: ["webkit", "moz", "MS", "o", ""],
-	classNames:["none","nano","micro","mini","norm","high","ultra"],
+	classNames:["zero-width","xx-small-width","x-small-width","small-width","medium-width","large-width","x-large-width","xx-large-width"],
 	classSizes: {
-        none: [0,0],
-        nano: [1,96],
-        micro: [97,288],
-        mini: [289,768],
-        norm: [769,1200],
-        high: [1201,1600],
-        ultra: [1601,10000]
+        "zero-width": [0,0],
+        "xx-small-width": [1,96],
+        "x-small-width": [97,288],
+        "small-width": [289,768],
+        "medium-width": [769,1200],
+        "large-width": [1201,1600],
+        "x-large-width": [1601,2560],
+        "xx-large-width": [2561,10000]
     },
     addClass: function (name,dimensions) {
     	this.classNames.push(name);
@@ -23,9 +24,14 @@ this.micronic = {
 			if (!prefixes[p]) type = type.toLowerCase();
 			window.addEventListener(prefixes[p]+type, this, true);
 		}
+		window.addEventListener("resize",this,true)
 	},
 	handleEvent: function (event) {
 		var target = event.target;
+		if (event.type=="resize") {
+			return this.recalc()
+		}
+		
 		if (target.hasAttribute("micronic")) {
 			var container = target.offsetParent;
 			var width = container.getBoundingClientRect().width,
@@ -33,29 +39,43 @@ this.micronic = {
 			var customDefs = target.getAttribute("micronic")
 			if (customDefs) {
 				var customDefinitions = Function("return {"+customDefs+"}")();
-				var disableDefault = customDefinitions.default === false;
-				delete customDefinitions.default;
+				var disableDefault = customDefinitions.$default === false;
+				var reflowing = customDefinitions.$reflowing;
+				delete customDefinitions.$reflowing
+				delete customDefinitions.$default;
 				Object.keys(customDefinitions).forEach(this.determineClassBySize.bind(this,target,classList,width,customDefinitions))
 
 			}
-			console.log(disableDefault)
 			if (!disableDefault) {
 			this.classNames.forEach(this.determineClassBySize.bind(this,target,classList,width,this.classSizes))
-	
+			}
+			if (reflowing) {
+				this.watch(target)
 			}
 			
 		}
 	},
+	watch: function (target) {
+		
+	},
 	determineClassBySize: function (target,classList,width,classSizes,className) {
 		var dimensions = classSizes[className];
+		if (typeof dimensions != "object") return;
 		if (width>=dimensions[0] && width <= dimensions[1]) {
 			classList.add(className)
-			console.log(className,dimensions,width)
 		} else {
 			classList.remove(className)
 		}
+	},
+	recalc: function () {
+		var micronics = document.querySelectorAll("[micronic]");
+		[].forEach.call(micronics,function (element) {
+			this.handleEvent({target:element})
+		},this)
 	}
 	
 
 
 }
+
+
